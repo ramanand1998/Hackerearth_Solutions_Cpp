495aacc72c1567505d115459bd4e3c49eb617941
ramanand <1998ramanandpatel@gmail.com>
2021-05-24 11:14:19 +0530

    Add math

---
+++ Math/Say no to Handshakes!!!.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        long long c=0;
+        n=n-1;
+        c=(n*(n+1))/2;
+        cout<<c<<endl; 
+    }
+}
